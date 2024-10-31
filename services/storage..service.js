d62282605d54bf8571d4251f568c1080daa0d8fa
Am1d1v <dimadiman1adima@gmail.com>
2024-10-31 15:34:27 +0300

    OS & Path

--- services/storage..service.js
+++ services/storage..service.js
@@ -1,8 +1,16 @@
 import {homedir} from 'os';
+import {join, basename, dirname, extname, relative, isAbsolute, resolve, sep} from 'path';
 
+const filePath = join(homedir(), 'weather-data.json');
 
 const saveKayValue = (key, value) => {
-    console.log(homedir());
+    console.log(basename(filePath));
+    console.log(dirname(filePath));
+    console.log(extname(filePath));
+    //console.log(relative(filePath), dirname(filePath));
+    console.log(isAbsolute(filePath));
+    console.log(resolve(filePath));
+    console.log(sep);
 };
 
 export {saveKayValue}
