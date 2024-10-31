662e8a3de40d0fc3b4b58c5b261b20508d225add
Am1d1v <dimadiman1adima@gmail.com>
2024-10-31 13:50:45 +0300

    Console output

--- weather.js
+++ weather.js
@@ -1,9 +1,26 @@
 #!/use/bin/env node
 import { getArgs } from "./helper/args.js";
-
+import { printError, printSuccess, printHelp } from "./services/log.service.js";
 
 const initCLI = () => {
     const args = getArgs(process.argv);
-    console.log(args);
+
+    // Help output
+    if(args.h){
+        printHelp();
+    }
+
+    // Save city
+    if(args.s){
+        
+    }
+
+    // Save token
+    if(args.t){
+        
+    }
+
+
+
 };
 initCLI();
