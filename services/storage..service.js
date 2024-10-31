1ba400563e6987a6355ea82b8fbfc0497e51f7f4
Am1d1v <dimadiman1adima@gmail.com>
2024-10-31 19:33:52 +0300

    Save/Get Value key

--- services/storage..service.js
+++ services/storage..service.js
@@ -1,16 +1,40 @@
 import {homedir} from 'os';
 import {join, basename, dirname, extname, relative, isAbsolute, resolve, sep} from 'path';
+import {writeFileSync, writeFile, promises} from 'fs';
 
 const filePath = join(homedir(), 'weather-data.json');
 
-const saveKayValue = (key, value) => {
-    console.log(basename(filePath));
-    console.log(dirname(filePath));
-    console.log(extname(filePath));
-    //console.log(relative(filePath), dirname(filePath));
-    console.log(isAbsolute(filePath));
-    console.log(resolve(filePath));
-    console.log(sep);
+const saveKayValue = async (key, value) => {
+    const data = {};
+
+    if(await isExist(filePath)){
+        const file = await promises.readFile(filePath);
+        data = JSON.parse(file);
+    }
+
+    data[key] = value;
+
+    await promises.writeFile(filePath, JSON.stringify(data));
 };
 
-export {saveKayValue}
+const getKeyValue = async (key) => {
+    if(await isExist(filePath)){
+        const file = await promises.readFile(filePath);
+        const data = JSON.parse(file);
+        return data[key];
+    }
+
+    return undefined;
+};
+
+// Check that the file is already exists
+const isExist = async (path) => {
+    try {
+        await promises.stat(path);
+        return true; 
+    } catch (error) {
+        return false;
+    }
+};
+
+export {saveKayValue, getKeyValue}
